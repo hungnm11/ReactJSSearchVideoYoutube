19790118cd8dccaafdaea6e6ca296497de561203
Hung Nguyen <hung@bacoor.co>
2017-07-12 13:21:23 +0700

    fix order css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import _ from 'lodash';
-import './assets/scss/style.scss';
+import React from 'react';
+import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'; 
+import './assets/scss/main.scss';
 
-console.log('Hello');
+class App extends React.Component {
+  render() {
+    return (
+      <h1 className="title">Hello</h1>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
+const bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+const extractSass = new ExtractTextPlugin('[name].style.css');
 const extractCSS = new ExtractTextPlugin('[name].bundle.css');
-const bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
-const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
-});
+
+// const extractSass = new ExtractTextPlugin({
+//     filename: "[name].[contenthash].css",
+//     disable: process.env.NODE_ENV === "development"
+// });
 
 module.exports = {
   entry: {
     app: './src/index.js',
-    vendor: ['lodash', 'bootstrap/dist/css/bootstrap.css'],
+    vendor: ['lodash'],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -21,7 +24,8 @@ module.exports = {
     }),
     new CleanWebpackPlugin(['dist']),
     new webpack.HotModuleReplacementPlugin(),
-    extractCSS
+    extractCSS,
+    extractSass,
   ],
   output: {
     filename: '[name].bundle.js',
@@ -35,19 +39,10 @@ module.exports = {
         exclude: [/node_modules/],
         use: [{
           loader: 'babel-loader',
-          options: {presets: ['es2015']},
+          options: {presets: ['es2015', 'react']},
         }],
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { module: true }
-          }
-        ]
-      },
+     
       {
         test: /\.less$/,
         use: [ 'style-loader', 'css-loader', 'less-loader' ]
@@ -66,14 +61,30 @@ module.exports = {
       {
         test: /\.(sass|scss)$/,
         use: extractSass.extract({
-                use: [{
-                    loader: "css-loader"
-                }, {
-                    loader: "sass-loader"
-                }],
-                // use style-loader in development
-                fallback: "style-loader"
-            })
+          use: [{
+              loader: "css-loader"
+          }, {
+              loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      },
+       {
+        test: /\.css$/,
+        // use: [
+        //   'style-loader',
+        //   {
+        //     loader: 'css-loader',
+        //     options: { module: true }
+        //   }
+        // ]
+        use: extractCSS.extract({
+          use: [{
+            loader: 'css-loader',
+          }],
+          fallback: 'style-loader'
+        })
       }
     ]
   },
