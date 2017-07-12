24ac00cf000a41423f8285e402268bd8b56ecfec
Hung Nguyen <hung@bacoor.co>
2017-07-12 15:20:59 +0700

    install eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Container from './container'
-import 'bootstrap/dist/css/bootstrap.css'; 
+import Container from './container';
+import 'bootstrap/dist/css/bootstrap.css';
 import './assets/scss/main.scss';
 
 render(<Container />, document.getElementById('app'));
