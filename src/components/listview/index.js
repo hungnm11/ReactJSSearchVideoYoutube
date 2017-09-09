07a73adf6d6169baeaaa33d211d571210903b0d1
Hung Nguyen <hung.nguyen@sentifi.com>
2017-09-09 22:17:56 +0700

    load the list wuth api

--- src/components/listview/index.js
+++ src/components/listview/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Collection, CollectionItem } from 'react-materialize';
 import { bindActionCreators } from 'redux';
 import * as actions from '../../actions';
@@ -21,23 +21,27 @@ class BodyUI extends Component {
   renderListView() {
     const { data: { items }, isFetching } = this.props.res;
     console.log('Hello');
-    // return items.map((item) => {
-    // console.log('ITEMS', item)
-    // return (
-    //   <CollectionItem 
-    //     key={item.id} 
-    //     onClick={this.selectedItemList.bind(null, item)}
-    //   >
-    //   <Link to={`/view/:${item.id}`}>
-    //   <span className="badge">{item.price}</span>
-    //     <div className='title'>
-    //       {item.title}
-    //     <span>{item.description}</span>
-    //     </div>  
-    //     </Link>       
-    //   </CollectionItem>
-    // )
-    // });
+    return items.map((item) => {
+    console.log('ITEMS', item)
+    const thumbImg = item.snippet.thumbnails.default.url;
+    return (
+      <CollectionItem 
+        key={item.etag} 
+        onClick={this.selectedItemList.bind(null, item)}
+      >
+      <NavLink to={`/view/${item.etag}`} >
+      <span className="badge">View</span>
+        <div className="thumbnails">
+          <img src={thumbImg} />
+        </div>
+        <div className='title'>
+          {item.snippet.title}
+        {/* <span>{item.snippet.description}</span> */}
+        </div>  
+        </NavLink>       
+      </CollectionItem>
+    )
+    });
   }
 
   render() {
